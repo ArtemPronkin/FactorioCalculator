3019991f1a4f56def025b4fcc568e715109ca9f6
Artyom <tema.ru2011@gmail.com>
2024-10-12 13:12:36 +0300

    SECOND commit

--- src/main/kotlin/Formula.kt
+++ src/main/kotlin/Formula.kt
@@ -216,6 +216,93 @@ data class SummaryBootle(override val count : Float) : Item(productionTime = 0F 
         RedBootles(count),
         BlueBootle(count),
         PurpleBootle(count),
+        YellowBootle(count),
     )
 )
 
+data class YellowBootle(override val count : Float) : Item(productionTime = 21F/3F ,
+    count = count,
+    formula = listOf(
+        Proccessor(count*2/3),
+        RobotFrame(count*1/3),
+        Structure(count*3/3),
+    )
+)
+
+data class Proccessor(override val count : Float) : Item(productionTime = 10F ,
+    count = count,
+    formula = listOf(
+        GreenElectricPlate(count*20),
+        RedElecticPlate(count*2),
+        SulfureAtid(count*5),
+    )
+)
+
+data class SulfureAtid(override val count : Float) : Item(productionTime = 1F/50F ,
+    count = count,
+    formula = listOf(
+        Iron(count*1/50),
+        Sulfur(count*5/50),
+        Water(count*100/50),
+    )
+)
+{
+    override val automatSpeed: Float = 1F
+}
+
+data class RobotFrame(override val count : Float) : Item(productionTime = 20F ,
+    count = count,
+    formula = listOf(
+        SteelPlate(count*1),
+        Battery(count*2),
+        GreenElectricPlate(count*3),
+        ElectricEngine(count*1),
+    )
+)
+
+data class ElectricEngine(override val count : Float) : Item(productionTime = 10F ,
+    count = count,
+    formula = listOf(
+        Engine(count*1),
+        GreenElectricPlate(count*2),
+        Lubricant(count*15)
+    )
+)
+
+data class Lubricant(override val count : Float) : Item(productionTime = 1F/10F ,
+    count = count,
+    formula = listOf(
+        HeavyOil(count*1),
+    )
+)
+{
+    override val automatSpeed: Float = 1F
+}
+
+data class HeavyOil(override val count : Float) : Item(productionTime = 0F ,
+    count = count,
+    formula = listOf(
+    )
+
+)
+
+data class Battery(override val count : Float) : Item(productionTime = 4F ,
+    count = count,
+    formula = listOf(
+        Iron(count*1),
+        Cooper(count*1),
+        SulfureAtid(count*20),
+    )
+)
+{
+    override val automatSpeed: Float = 1F
+}
+
+data class Structure(override val count : Float) : Item(productionTime = 20F ,
+    count = count,
+    formula = listOf(
+        Cooper(count*20),
+        SteelPlate(count*2),
+        Plastic(count*5),
+    )
+)
